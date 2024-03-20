0eec413f4fb7efddc82766d25b7d17cc8dd1827a
aaronpan-eng <91743944+aaronpan-eng@users.noreply.github.com>
2024-03-20 00:10:41 -0400

    added code to generate sphere

--- include/aaron.h
+++ include/aaron.h
@@ -1,7 +1,11 @@
 #pragma once
+
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <stdio.h>
 #include <cmath>
+#include <math.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
@@ -16,3 +20,5 @@ using namespace cv;
 int capture_cal_images();
 
 int select_images(vector<Point2f> corner_set, vector<vector<Point2f>> &corner_list, vector<Vec3f> &point_set, vector<vector<Vec3f>> &point_list);
+
+int generate_sphere_points(int N, float radius, vector<float> origin, vector<Point3f> &objectPoints);
